4694b26c8a4fed21852a7a51b76715751bb1e700
A-d-i-b <adibsiddiqui720@gmail.com>
2023-05-31 21:53:08 +0530

    Fixed errors

--- lib/components/BMRoadAssComponent.dart
+++ lib/components/BMRoadAssComponent.dart
@@ -10,29 +10,27 @@ class BMRoadAssComponent extends StatefulWidget {
   bool fullScreenComponent;
 
   BMRoadAssComponent(
-      {required this.element,
-      required this.fullScreenComponent});
+      {required this.element, required this.fullScreenComponent});
 
   @override
   State<BMRoadAssComponent> createState() => _BMRoadAssComponentState();
 }
 
 class _BMRoadAssComponentState extends State<BMRoadAssComponent> {
-
-  _callNumber(String number) async{
-  bool? res = await FlutterPhoneDirectCaller.callNumber(number);
-}
+  _callNumber(String number) async {
+    bool? res = await FlutterPhoneDirectCaller.callNumber(number);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: widget.fullScreenComponent ? context.width() - 32 : 250,
+      // width: widget.fullScreenComponent ? context.width() - 32 : 250,
       decoration:
           BoxDecoration(color: context.cardColor, borderRadius: radius(32)),
       child: Stack(
         children: [
           Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               bmCommonCachedNetworkImage(
                 widget.element.image,
@@ -43,11 +41,25 @@ class _BMRoadAssComponentState extends State<BMRoadAssComponent> {
               8.height,
               Row(
                 children: [
-                  Expanded(child: Text(widget.element.name, style: TextStyle(fontSize: 22, color: bmSpecialColorDark),),),
-                  Expanded(child: SizedBox(width: 1,)),
-                  IconButton(onPressed: (){
-                    _callNumber(widget.element.number.toString());
-                  }, icon: Icon(Icons.call, color: bmSpecialColorDark,))
+                  SizedBox(width: 12),
+                  Expanded(
+                    child: Text(
+                      widget.element.name,
+                      style: TextStyle(fontSize: 22, color: bmSpecialColorDark),
+                    ),
+                  ),
+                  Expanded(
+                      child: SizedBox(
+                    width: 1,
+                  )),
+                  IconButton(
+                      onPressed: () {
+                        _callNumber(widget.element.number.toString());
+                      },
+                      icon: Icon(
+                        Icons.call,
+                        color: bmSpecialColorDark,
+                      ))
                 ],
               )
             ],
