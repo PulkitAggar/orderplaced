73a2c6c97eddf5b435ee9533a1824c1bfaa636bb
PulkitAgga <devilmaestro123456789@gmail.com>
2023-06-03 16:20:36 +0530

    ui changes

--- lib/screens/past_order_screen.dart
+++ lib/screens/past_order_screen.dart
@@ -26,7 +26,7 @@ class _PastOrdersState extends State<PastOrders> {
         shadowColor: Colors.transparent,
         backgroundColor: Colors.transparent,
         title: Text(
-          'Current Orders',
+          'Order History',
           style: boldTextStyle(color: Colors.black, size: 24),
         ),
       ),
@@ -40,89 +40,92 @@ class _PastOrdersState extends State<PastOrders> {
                   return Column(
                       mainAxisSize: MainAxisSize.max,
                       children: snapshot.data!.map((e) {
-                        return Card(
-                          color: Colors.grey.shade200,
-                            shape: ContinuousRectangleBorder(
-                                borderRadius: BorderRadius.circular(24)),
-                                clipBehavior: Clip.antiAliasWithSaveLayer,
-                            elevation: BorderSide.strokeAlignOutside,
-                            child: Container(
-                              // height: 300.0,
-                              decoration: BoxDecoration(color: Colors.grey.shade200),
-                              width: double.infinity,
-                              // padding: EdgeInsets.all(8.0), YOU CAN DO thIS YOU KNOW that RIGHT??????????????????????
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.start,
-                                  children: [
-                                    ExpansionTile(
-                                      collapsedIconColor: Colors.black,
-                                      title: Column(
-                                        mainAxisSize: MainAxisSize.max,
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Text(e.storeName, style: TextStyle(fontWeight: FontWeight.w900, fontSize: 18)),
-                                          Space(4),
-                                          Container(
-                                            width: double.infinity,
-                                            child: Row(
-                                              mainAxisAlignment: MainAxisAlignment.start,
-                                              crossAxisAlignment: CrossAxisAlignment.center,
-                                              children: [
-                                                Icon(Icons.watch_later_outlined, color: Colors.orange, size: 16),
-                                                Space(2),
-                                                Text(e.date, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-                                                Space(2),
-                                                Text("at", style: TextStyle(color: Colors.orange, fontSize: 12)),
-                                                Space(2),
-                                                Text(e.time, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-                                                Expanded(child: SizedBox(width: 1,)),
-                                                Text(
-                                                    e.orderStatus,
-                                                    textAlign: TextAlign.start,
-                                                    style: TextStyle(
-                                                      color: e.orderStatus == "placed" ? Colors.orange : blueColor,
-                                                      fontWeight: FontWeight.w500,
-                                                      fontSize: 16,
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            color: Colors.grey.shade200,
+                              shape: ContinuousRectangleBorder(
+                                  borderRadius: BorderRadius.circular(24)),
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                              elevation: BorderSide.strokeAlignOutside,
+                              child: Container(
+                                // height: 300.0,
+                                decoration: BoxDecoration(color: Colors.grey.shade200),
+                                width: double.infinity,
+                                // padding: EdgeInsets.all(8.0), YOU CAN DO thIS YOU KNOW that RIGHT??????????????????????
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      ExpansionTile(
+                                        collapsedIconColor: Colors.black,
+                                        title: Column(
+                                          mainAxisSize: MainAxisSize.max,
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Text(e.storeName, style: TextStyle(fontWeight: FontWeight.w900, fontSize: 18)),
+                                            Space(4),
+                                            Container(
+                                              width: double.infinity,
+                                              child: Row(
+                                                mainAxisAlignment: MainAxisAlignment.start,
+                                                crossAxisAlignment: CrossAxisAlignment.center,
+                                                children: [
+                                                  Icon(Icons.watch_later_outlined, color: Colors.orange, size: 16),
+                                                  Space(2),
+                                                  Text(e.date, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                                                  Space(2),
+                                                  Text("at", style: TextStyle(color: Colors.orange, fontSize: 12)),
+                                                  Space(2),
+                                                  Text(e.time, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                                                  Expanded(child: SizedBox(width: 1,)),
+                                                  Text(
+                                                      e.orderStatus,
+                                                      textAlign: TextAlign.start,
+                                                      style: TextStyle(
+                                                        color: e.orderStatus == "placed" ? Colors.orange : blueColor,
+                                                        fontWeight: FontWeight.w500,
+                                                        fontSize: 16,
+                                                      ),
                                                     ),
-                                                  ),
-                                              ],
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      children: [
-                                        ListView.builder(
-                                          shrinkWrap: true,
-                                          itemCount: e.map.length,
-                                          itemBuilder: (context, index) {
-                                            String key =
-                                                e.map.keys.elementAt(index);
-                                            Map<String, dynamic> item =
-                                                e.map[key];
-                                            double cost = item['cost'];
-                                            int count = item['count'];
-                                            String imageUrl = item['image'];
-
-                                            return ListTile(
-                                              leading: Image.network(imageUrl),
-                                              title: Text('Item: $key'),
-                                              subtitle: Text(
-                                                  'Cost: $cost, Count: $count'),
-                                            );
-                                          },
+                                                ],
+                                              ),
+                                            )
+                                          ],
                                         ),
-                                      ],
-                                    )
-                                  ],
+                                        children: [
+                                          ListView.builder(
+                                            shrinkWrap: true,
+                                            itemCount: e.map.length,
+                                            itemBuilder: (context, index) {
+                                              String key =
+                                                  e.map.keys.elementAt(index);
+                                              Map<String, dynamic> item =
+                                                  e.map[key];
+                                              double cost = item['cost'];
+                                              int count = item['count'];
+                                              String imageUrl = item['image'];
+                        
+                                              return ListTile(
+                                                leading: Image.network(imageUrl),
+                                                title: Text('Item: $key'),
+                                                subtitle: Text(
+                                                    'Cost: $cost, Count: $count'),
+                                              );
+                                            },
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ));
+                              )),
+                        );
                       }).toList());
                 } else if (snapshot.hasError) {
                   return Text('Error: ${snapshot.error}');
